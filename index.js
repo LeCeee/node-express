0031b0deb86ac5315a0c07202fbd2188c1ab1bd0
queenCee <aakarshitatiwari@gmail.com>
2018-06-04 23:13:56 +0530

    Express Serve Static Files

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const http = require('http');
 const port = 3000;
 const hostname = 'localhost';
+const morgan = require('morgan');
 const app = express();
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
 
 app.use((req,res,next) => {
     console.log(req.headers);
